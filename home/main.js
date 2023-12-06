608b9a3d5fa03db268459150790ab329fae1d368
MardonbeKhursanov <mrkhankhursanov@gmail.com>
2023-12-06 11:12:44 +0300

    yarmi

---
+++ home/main.js
@@ -0,0 +1,9 @@
+
+
+let menu = document.getElementById("hamburger")
+let btn = document.getElementById('toggle')
+
+
+btn.addEventListener('click', function(){
+    menu.classList.toggle('active')
+})
